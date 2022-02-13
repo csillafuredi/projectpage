2ba4beac16ef738c69ee63d1151ed107d0aba940
csillafuredi <82845800+csillafuredi@users.noreply.github.com>
2022-02-13 12:54:13 +0000

    cleaning up the code, adding jquery

--- main.js
+++ main.js
@@ -3,5 +3,12 @@ $(document).ready(function(){
       $("#panel").slideToggle("slow");
       $("#down").toggleClass("fa-rotate-180"); 
     });
+
+    $('#view-work').on('click', function (){
+      const images = $('#images').position().top;
+      $('html, body').animate({
+        scrollTop: images
+      }, 900);
+    });
     
   });
